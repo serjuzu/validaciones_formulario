9c5a34f6b2bce466e8f1baf9f4ed968ca3cd8c59
serjuzu <serjuzu@gmail.com>
2023-06-30 12:38:55 +0200

    -- import.sql con datos de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO alumnos (nombre, apellidos, edad, curso, fecha_ingreso) VALUES('Alfredo', 'Jiménez Rivera', 36, 'GraphQL', '2023-06-29');
+INSERT INTO alumnos (nombre, apellidos, edad, curso, fecha_ingreso) VALUES('Rubén', 'Ben Levy', 36, 'GraphQL', '2023-06-29');
+INSERT INTO alumnos (nombre, apellidos, edad, curso, fecha_ingreso) VALUES('Ana', 'Rodríguez Menéndez', 26, 'Diseño con Figma', '2023-07-04');
+INSERT INTO alumnos (nombre, apellidos, edad, curso, fecha_ingreso) VALUES('Julián', 'Contreras López', 33, 'Adobe XD', '2023-06-21');
+INSERT INTO alumnos (nombre, apellidos, edad, curso, fecha_ingreso) VALUES('Brian', 'Pérez Nabayón', 18, 'Photoshop', '2023-06-02');
+INSERT INTO alumnos (nombre, apellidos, edad, curso, fecha_ingreso) VALUES('Rafa', 'Guterres Tarimez', 25, 'Autocad', '2023-06-29');
